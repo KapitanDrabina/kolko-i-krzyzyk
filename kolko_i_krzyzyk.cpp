5274891546665974b32c882507f7ab0dc884c03e
KapitanDrabina <168362906+KapitanDrabina@users.noreply.github.com>
2024-06-30 12:12:00 +0200

    Update kolko_i_krzyzyk.cpp

--- kolko_i_krzyzyk.cpp
+++ kolko_i_krzyzyk.cpp
@@ -8,18 +8,19 @@ int main(){
     char move = 'X';
     string board[] = {"1","2","3","4","5","6","7","8","9"};
     for(int i = 1; i<=9;i++){
+        
         print(board);
 
         cout << "\nPlayer " << move << "\n" << "\nEnter the number of the field to be marked: ";
-        int zaznaczenie;
-        cin >> zaznaczenie;
+        int square;
+        cin >> square;
 
         if(move == 'X'){
-            board[zaznaczenie-1] = "X";
+            board[square-1] = "X";
             move = 'O';
         }
         else{
-            board[zaznaczenie-1] = "O";
+            board[square-1] = "O";
             move = 'X';
         }
         if(check(board) == 'X'){
@@ -30,11 +31,11 @@ int main(){
         else if(check(board) == 'O'){
             print(board);
             cout << "\n" << "O Won!";
-
             return 0;
         }
     }
-    cout << board << "\n" << "It's a draw!";
+    print(board);
+    cout << "\n" << "It's a draw!";
     return 0;
 }
 
@@ -48,27 +49,23 @@ void print(string board[]){
 
 char check(string board[]){
     for(int i = 0; i < 3; i++){
-        if(board[i] == board[i+3]){
-            if(board[i+3] == board[i=6]){
-                if(board[i] == "X"){
-                    return 'X';
-                }
-                else{
-                    return 'O';
-                }
+        if(board[i] == board[i+3] and board[i+3] == board[i+6]){
+            if(board[i] == "X"){
+                return 'X';
+            }
+            else{
+                return 'O';
             }
         }
     }
 
     for(int i = 0; i < 7; i = i + 3){
-        if(board[i] == board[i+1]){
-            if(board[i+1] == board[i+2]){
-                if(board[i] == "X"){
-                    return 'X';
-                }
-                else{
-                    return 'O';
-                }
+        if(board[i] == board[i+1] and board[i+1] == board[i+2]){
+            if(board[i] == "X"){
+                return 'X';
+            }
+            else{
+                return 'O';
             }
         }
     }
